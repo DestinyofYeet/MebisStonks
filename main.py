cfd0442722d0739f392e784a5437cef58c90415c
DestinyofYeet <olebend@gmail.com>
2020-12-08 21:13:10 +0100

    Update go brrr

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-from MebisStonks.tools import interface
+from tools.interface import Interface
 import json
+from datetime import datetime
+import time
+import os
 
 
 def load_userdata():
+    # loads userdata
     with open("config/userdata.json") as f:
         config = json.load(f)
 
@@ -10,6 +14,7 @@ def load_userdata():
 
 
 def load_config():
+    # loads config
     with open("config/config.json") as f:
         config = json.load(f)
 
@@ -18,6 +23,22 @@ def load_config():
 
 username, password = load_userdata()
 abgabe_url = load_config()
-bot = interface.Interface(username, password, abgabe_url, debug=True)
 
+# checks if time is between 8:00 and 8:15 o'clock to log you in and to check you off
+
+while True:
+    if int(datetime.now().strftime("%H")) == 8 and 0 < int(datetime.now().strftime("%M")) < 15:
+        print("My time has come...")
+        break
+    else:
+        print(f"Waiting 5 more mins, current time is {datetime.now().strftime('%H:%M')}")
+        time.sleep(300)
+
+
+# starts the main browser, the debug option just tells the script to show the chrome screen or to not show it
+bot = Interface(username, password, abgabe_url, debug=True)
+
+
+bot.main_program()
+bot.download_page()
 bot.close_driver()
--- tools/interface.py
+++ tools/interface.py
@@ -16,23 +16,29 @@ class Interface:
         self.start_driver()
 
     def start_driver(self):
+        # starts the driver
         options = webdriver.ChromeOptions()
         if not self.debug:
+            # either show or not show the chrome-driver
             options.add_argument('headless')
+        # just some options to make it more smoothly and to make it output less
         options.add_argument("--log-level=3")
         options.add_argument("--no-sandbox")
         options.add_argument("start-maximized")
         options.add_argument('--disable-dev-shm-usage')
         options.add_experimental_option("excludeSwitches", ["enable-logging"])
 
+        # routes the chromedriver output to the corresponding nul output
         if sys.platform.startswith("win"):
-            self.driver = webdriver.Chrome(options=options, service_log_path='NUL')
+            self.driver = webdriver.Chrome("chromedriver.exe", options=options, service_log_path='NUL')
         else:
-            self.driver = webdriver.Chrome(options=options, service_log_path='/dev/null')
+            self.driver = webdriver.Chrome("chromedriver", options=options, service_log_path='/dev/null')
 
+        # goes to self.url
         self.driver.get(self.url)
+
+        # logs in
         self.perform_login()
-        self.main_program()
 
     def perform_login(self):
         # tabs to the 'insert username field'
@@ -54,8 +60,18 @@ class Interface:
         action.perform()
         time.sleep(0.5)
 
+    def download_page(self, path=None):
+        # checks if a specific path is given, if not, its just downloading the webpage to the main directory
+        if path is None:
+            with open("page.html", 'w+', encoding='utf-8') as f:
+                f.write(self.driver.page_source)
+
     def main_program(self):
+        # main program
+
+        # goes to the url its supposed to check that you were there
         self.driver.get(self.abgabe_url)
 
     def close_driver(self):
+        # closes the driver
         self.driver.close()
